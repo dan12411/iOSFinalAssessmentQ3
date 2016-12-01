d9a4836068eaec0e0fe037960ebf4253c253c575
TeChengHung <dan12411@gmail.com>
2016-12-01 21:12:34 +0800

    Finish

--- README.md
+++ README.md
@@ -9,9 +9,9 @@
 
 實作過程：
 
-1. 撰寫一個程式發 GET 到 https://httpbin.org/get, 接收回傳資料並把 origin 的值用 NSLog 印出來
+1. 撰寫一個程式發 GET 到 https://httpbin.org/get, 接收回傳資料並把 origin 的值用 NSLog 印出來。
 
-2. 撰寫一個程式發 POST 到 https://httpsbin.org/post, post 的 body 是 JSON 格式
+2. 撰寫一個程式發 POST 到 https://httpsbin.org/post, post 的 body 是 JSON 格式: {"time" : "2016-07-15" 12:00:00}, "2016-07-15 12:00:00" 替換為送出 POST 的時間，並且用 NSDateFormatter 產生出上述的格式，把收到回傳時的時間，跟 time 值作出比較，算出 request 花的時間，用 NSLog 以"花了xx秒"的格式印出來。
 
 
 ---
--- iOSFinalAssessmentQ3/ViewController.swift
+++ iOSFinalAssessmentQ3/ViewController.swift
@@ -10,9 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    // GET data
+    @IBAction func getAction(_ sender: Any) {
         // get url
         let url = URL(string: "https://httpbin.org/get")
         var urlRequest = URLRequest(url: url!)
@@ -36,6 +35,71 @@ class ViewController: UIViewController {
         })
         
         task.resume()
+    }
+    
+    // POST data
+    @IBAction func postAction(_ sender: Any) {
+        let url = URL(string: "https://httpbin.org/post")
+        var urlRequest = URLRequest(url: url!)
+        
+        urlRequest.httpMethod = "POST"
+        
+        // get current time
+        let currentTime = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        // let time transfer to string
+        let str = dateFormatter.string(from: currentTime)
+//        print("currentTime: \(str)")
+        // prepare data
+        let postData = ["time": str]
+//        print(postData)
+        let jsonData = try! JSONSerialization.data(withJSONObject: postData, options: .prettyPrinted)
+        urlRequest.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: urlRequest){ data, response, error in
+            if error != nil{
+                print("Error -> \(error?.localizedDescription)")
+                return
+            }
+            
+//            let result = try! JSONSerialization.jsonObject(with: data!, options: []) as? [String:AnyObject]
+//            
+//            print("Result -> \(result)")
+            
+            // get response time
+            let responseTime = Date()
+//            print("responseTime: \(responseTime)")
+            let transferTime = responseTime.timeIntervalSince(currentTime)
+            
+            NSLog("花了\(transferTime)秒")
+            
+        }
+        
+//        let postStr = str
+//        
+//        let data = postStr.data(using: String.Encoding.utf8)
+//        
+//        let task2 = URLSession.shared.uploadTask(with: urlRequest, from: data) {
+//            (data:Data?, response:URLResponse?, err:Error?) -> Void in
+//            
+//            if err != nil {
+//                print("===\(err?.localizedDescription)")
+//            }
+//            
+//            // get response time
+//            let responseTime = Date()
+//            print("responseTime: \(responseTime)")
+//            let transferTime = responseTime.timeIntervalSince(currentTime)
+//            
+//            NSLog("花了\(transferTime)秒")
+//        }
+        
+        task.resume()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
     
     override func didReceiveMemoryWarning() {
